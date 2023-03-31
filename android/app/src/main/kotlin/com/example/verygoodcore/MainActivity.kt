b9712e063a28fbdd95336e161dc42129e6c76c26
Woloyor <yuralysyshak@gmail.com>
2023-03-31 20:10:25 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.audio_school
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:audio_school/app/app.dart';
+import 'package:audio_school/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
